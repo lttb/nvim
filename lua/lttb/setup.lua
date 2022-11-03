f10625eb41422d018e416122311b28be4df61c82
lttb <kenzhaev.artur@gmail.com>
2022-11-03 00:48:33 +0000

    feat: add substitute.nvim

--- lua/lttb/setup.lua
+++ lua/lttb/setup.lua
@@ -80,6 +80,19 @@ require('packer').startup(function(use)
   })
 
   use({
+    'gbprod/substitute.nvim',
+    config = function()
+      local substitute = require('substitute')
+      substitute.setup({})
+
+      vim.keymap.set('n', 'r', substitute.operator, { noremap = true })
+      vim.keymap.set('n', 'rr', require('substitute').line, { noremap = true })
+      vim.keymap.set('n', 'R', require('substitute').eol, { noremap = true })
+      vim.keymap.set('x', 'r', require('substitute').visual, { noremap = true })
+    end,
+  })
+
+  use({
     'phaazon/hop.nvim',
     branch = 'v2',
     config = function()
