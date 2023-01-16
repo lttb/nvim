2ba01920657a8fa77f14b4285c00989b221c1b1b
lttb <kenzhaev.artur@gmail.com>
2023-01-16 18:38:37 +0000

    feat(lualine): ignore nvim-tree

--- lua/lttb/plugins/lualine.lua
+++ lua/lttb/plugins/lualine.lua
@@ -5,28 +5,29 @@ local function config()
   local theme = require('lttb.theme')
 
   require('lualine').setup({
-    disabled_filetypes = { 'NvimTree' },
 
     options = {
       icons_enabled = true,
       theme = 'auto',
       -- component_separators = '|',
       -- section_separators = '',
+
+      disabled_filetypes = { 'NvimTree' },
     },
 
-    sections = {
-      -- lualine_c = {
-      --   {
-      --     'filename',
-      --     newfile_status = true,
-      --     path = 1,
-      --   },
-      --   -- {
-      --   --   git_blame.get_current_blame_text,
-      --   --   cond = git_blame.is_blame_text_available,
-      --   -- },
-      -- },
-    },
+    -- sections = {
+    --   -- lualine_c = {
+    --   --   {
+    --   --     'filename',
+    --   --     newfile_status = true,
+    --   --     path = 1,
+    --   --   },
+    --   --   -- {
+    --   --   --   git_blame.get_current_blame_text,
+    --   --   --   cond = git_blame.is_blame_text_available,
+    --   --   -- },
+    --   -- },
+    -- },
   })
 end
 
