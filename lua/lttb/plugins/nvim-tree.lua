5033260f25d9a1c415c81a574a0b379b76b4203e
lttb <kenzhaev.artur@gmail.com>
2023-01-30 12:12:15 +0000

    feat(nvim-tree): update open on startup

--- lua/lttb/plugins/nvim-tree.lua
+++ lua/lttb/plugins/nvim-tree.lua
@@ -7,9 +7,7 @@ local function config()
     -- disable_netrw = false,
     -- hijack_netrw = false,
 
-    open_on_setup = true,
-    open_on_setup_file = false,
-    ignore_ft_on_setup = { 'gitcommit', 'gitrebase' },
+    -- ignore_ft_on_setup = { 'gitcommit', 'gitrebase' },
     -- respect_buf_cwd = true,
     sync_root_with_cwd = true,
     reload_on_bufenter = true,
@@ -62,7 +60,6 @@ local function config()
   })
 
   local nt_api = require('nvim-tree.api')
-  local utils = require('lttb.utils')
 
   utils.keyplug('lttb-sidebar-toggle', function()
     nt_api.tree.toggle(true, true)
@@ -71,6 +68,38 @@ local function config()
   utils.keyplug('lttb-sidebar-focus', function()
     nt_api.tree.focus()
   end)
+
+  -- @see https://github.com/nvim-tree/nvim-tree.lua/wiki/Open-At-Startup
+  local function open_nvim_tree(data)
+    local IGNORED_FT = {
+      'gitcommit',
+      'gitrebase',
+    }
+
+    -- buffer is a real file on the disk
+    local real_file = vim.fn.filereadable(data.file) == 1
+
+    -- buffer is a [No Name]
+    local no_name = data.file == '' and vim.bo[data.buf].buftype == ''
+
+    -- &ft
+    local filetype = vim.bo[data.buf].ft
+
+    -- only files please
+    if not real_file and not no_name then
+      return
+    end
+
+    -- skip ignored filetypes
+    if vim.tbl_contains(IGNORED_FT, filetype) then
+      return
+    end
+
+    -- open the tree but don't focus it
+    require('nvim-tree.api').tree.toggle({ focus = false })
+  end
+
+  vim.api.nvim_create_autocmd({ 'VimEnter' }, { callback = open_nvim_tree })
 end
 
 if utils.is_vscode() then
