3791fd1b0725901db2371dee84a63e9a32c7fbcf
lttb <kenzhaev.artur@gmail.com>
2023-03-31 20:58:35 +0100

    feat(plugins): reconfigure luasnip

--- lua/lttb/plugins/lsp.lua
+++ lua/lttb/plugins/lsp.lua
@@ -80,6 +80,12 @@ local function config()
   end
 
   -- CMP {{{
+  local has_words_before = function()
+    unpack = unpack or table.unpack
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+  end
+
   local cmp = require('cmp')
   local lspkind = require('lspkind')
   local luasnip = require('luasnip')
@@ -97,9 +103,16 @@ local function config()
       ['<C-d>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete({}),
-      ['<CR>'] = cmp.mapping.confirm({
-        behavior = cmp.ConfirmBehavior.Replace,
-        select = true,
+      ['<CR>'] = cmp.mapping({
+        i = function(fallback)
+          if cmp.visible() and cmp.get_active_entry() then
+            cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+          else
+            fallback()
+          end
+        end,
+        s = cmp.mapping.confirm({ select = true }),
+        c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
       }),
       ['<Tab>'] = cmp.mapping(function(fallback)
         -- support copilot
@@ -108,8 +121,12 @@ local function config()
           require('copilot.suggestion').accept()
         elseif cmp.visible() then
           cmp.select_next_item()
+        -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+        -- they way you will only jump inside the snippet region
         elseif luasnip.expand_or_jumpable() then
           luasnip.expand_or_jump()
+        elseif has_words_before() then
+          cmp.complete()
         else
           fallback()
         end
@@ -296,9 +313,16 @@ return {
 
       'onsails/lspkind.nvim',
 
-      'saadparwaiz1/cmp_luasnip',
-      'L3MON4D3/LuaSnip',
-
+      {
+        'L3MON4D3/LuaSnip',
+        -- follow latest release.
+        version = '<CurrentMajor>.*',
+        -- install jsregexp (optional!).
+        build = 'make install_jsregexp',
+        dependencies = {
+          'saadparwaiz1/cmp_luasnip',
+        },
+      },
       'lukas-reineke/cmp-rg',
     },
     config = config,
