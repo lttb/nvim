28588d71036aaa0f6e1913ba46cff301a4f554c0
lttb <kenzhaev.artur@gmail.com>
2023-05-11 01:08:02 +0100

    feat(neovide): enable noice

--- lua/lttb/plugins/init.lua
+++ lua/lttb/plugins/init.lua
@@ -241,7 +241,7 @@ return vim.list_extend(
       dependencies = {
         'MunifTanjim/nui.nvim',
       },
-      enabled = not utils.is_neovide(),
+      -- enabled = not utils.is_neovide(),
     },
 
     {
