784aaf29fcde97acb70fc8c3adf4bbd29ce58557
lttb <kenzhaev.artur@gmail.com>
2023-05-18 16:12:27 +0100

    feat(neovide): update settings

--- lua/lttb/settings/neovide.lua
+++ lua/lttb/settings/neovide.lua
@@ -12,7 +12,8 @@ vim.opt.guifont = 'Fira Code:h15:#e-subpixelantialias:#h-none'
 if theme.colorscheme == 'github_dark' then
   vim.g.neovide_background_color = '#23292f'
 elseif theme.colorscheme == 'github_light' then
-  vim.g.neovide_background_color = '#f7f9fb'
+  -- vim.g.neovide_background_color = '#f7f9fb'
+  vim.g.neovide_background_color = '#ffffff'
 elseif theme.colorscheme == 'catppuccin-frappe' then
   vim.g.neovide_background_color = '#2f3446'
 end
@@ -22,15 +23,16 @@ vim.g.neovide_floating_blur_amount_y = 2.0
 
 vim.g.neovide_fullscreen = false
 
-vim.g.neovide_remember_window_size = true
-vim.g.neovide_remember_window_position = true
+-- vim.g.neovide_remember_window_size = true
+-- vim.g.neovide_remember_window_position = true
 
 vim.g.neovide_input_macos_alt_is_meta = true
 vim.g.neovide_input_use_logo = true
 
-vim.g.neovide_padding_top = 24
-vim.g.neovide_padding_left = 24
-vim.g.neovide_padding_right = 24
+-- vim.g.neovide_padding_top = 24
+vim.g.neovide_padding_bottom = 0
+vim.g.neovide_padding_right = 0
+-- vim.g.neovide_padding_left = 1
 
 -- vim.opt.linespace = 2
 
