e75526c1674578830ff1e836dda7e5a4819881a4
lttb <kenzhaev.artur@gmail.com>
2023-05-19 01:03:46 +0100

    feat: add telescope-fzy-native to optimise smart-open

--- lua/lttb/plugins/telescope.lua
+++ lua/lttb/plugins/telescope.lua
@@ -161,6 +161,13 @@ return {
       },
 
       {
+        'nvim-telescope/telescope-fzy-native.nvim',
+        config = function()
+          require('telescope').load_extension('fzy_native')
+        end,
+      },
+
+      {
         'nvim-telescope/telescope-file-browser.nvim',
         config = function()
           require('telescope').load_extension('file_browser')
