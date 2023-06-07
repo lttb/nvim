0025478cbebd49cc3736daddde055b87c7d39308
lttb <kenzhaev.artur@gmail.com>
2023-06-07 12:13:47 +0100

    feat(neovide): disable copy/paste keybindings

--- lua/lttb/settings/neovide.lua
+++ lua/lttb/settings/neovide.lua
@@ -39,7 +39,7 @@ vim.g.neovide_padding_right = 0
 -- vim.g.neovide_profiler = true
 -- vim.g.neovide_refresh_rate = 144
 
-vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true })
