6af8cea4667ef0577cb1c1e3931430eb7625566f
Artur Kenzhaev <kenzhaev.artur@gmail.com>
2023-08-22 15:05:42 +0100

    feat(neotree): use nested in autocmd to hide number col

--- lua/lttb/plugins/neo-tree.lua
+++ lua/lttb/plugins/neo-tree.lua
@@ -40,6 +40,9 @@ local function config()
   end)
 
   vim.api.nvim_create_autocmd({ 'VimEnter' }, {
+    -- it should be "nested" not to show the number column
+    -- @see https://github.com/nvim-neo-tree/neo-tree.nvim/issues/1106
+    nested = true,
     callback = function(data)
       if not utils.should_open_sidebar(data) then
         return
