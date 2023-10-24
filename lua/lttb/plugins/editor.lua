87b77116f2b65a539217d5b72544e5291f0353b9
Artur Kenzhaev <kenzhaev.artur@gmail.com>
2023-10-24 19:54:04 +0100

    feat: add neovim-project plugin (disabled)

--- lua/lttb/plugins/editor.lua
+++ lua/lttb/plugins/editor.lua
@@ -337,6 +337,14 @@ return {
           },
 
           separator_style = 'thin',
+
+          hover = {
+            enabled = true,
+            delay = 200,
+            reveal = { 'close' },
+          },
+
+          diagnostics = 'nvim_lsp',
         },
 
         highlights = theme.variant == 'light' and {
@@ -353,4 +361,25 @@ return {
 
     -- enabled = false,
   },
+
+  -- {
+  --   'coffebar/neovim-project',
+  --   opts = {
+  --     projects = { -- define project roots
+  --       '~/dev/work/*',
+  --       '~/.config/*',
+  --     },
+  --   },
+  --   init = function()
+  --     -- enable saving the state of plugins in the session
+  --     vim.opt.sessionoptions:append('globals') -- save global variables that start with an uppercase letter and contain at least one lowercase letter.
+  --   end,
+  --   dependencies = {
+  --     { 'nvim-lua/plenary.nvim' },
+  --     { 'nvim-telescope/telescope.nvim', tag = '0.1.4' },
+  --     { 'Shatur/neovim-session-manager' },
+  --   },
+  --   lazy = false,
+  --   priority = 100,
+  -- },
 }
