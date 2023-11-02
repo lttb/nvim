e20fc739dab7795fc87535ea5d6aacda38d00198
Artur Kenzhaev <kenzhaev.artur@gmail.com>
2023-11-02 23:47:24 +0000

    feat: return wordmotion prefix for shortcuts

--- lua/lttb/keymaps.lua
+++ lua/lttb/keymaps.lua
@@ -38,19 +38,21 @@ local function native_nav(key, ncmd, icmd, xcmd)
   end
 end
 
+local wm = ';'
+
 native_nav('<M-BS>', 'ldb', 'i')
-native_nav('<M-DEL>', 'edaw', 'i')
+native_nav('<M-DEL>', wm .. 'eda' .. wm .. 'w', 'i')
 
 native_nav('<S-Left>', 'vh', '', 'h')
 native_nav('<S-Right>', 'vl', '', 'l')
 native_nav('<S-Up>', 'vk', '', 'k')
 native_nav('<S-Down>', 'vj', '', 'j')
 
-native_nav('<M-Left>', 'b', 'i')
-native_nav('<M-Right>', 'e', 'a')
+native_nav('<M-Left>', wm .. 'b', 'i')
+native_nav('<M-Right>', wm .. 'e', 'a')
 
-native_nav('<M-S-Left>', 'evb', '', ',b')
-native_nav('<M-S-Right>', 'vie', '', ',w')
+native_nav('<M-S-Left>', wm .. 'ev' .. wm .. 'b', '', ',b')
+native_nav('<M-S-Right>', 'vi' .. wm .. 'e', '', ',w')
 -- }}}
 
 -- {{{ Global mappings
