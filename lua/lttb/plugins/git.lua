ff992d174b5a670d932e726016f413b8612076c4
Artur Kenzhaev <kenzhaev.artur@gmail.com>
2023-11-04 11:49:47 +0000

    feat(gitsigns): update options

--- lua/lttb/plugins/git.lua
+++ lua/lttb/plugins/git.lua
@@ -21,9 +21,13 @@ return {
     'lewis6991/gitsigns.nvim',
     event = 'VimEnter',
     opts = {
+      yadm = { enable = true },
+
       current_line_blame = true,
       current_line_blame_opts = {
         delay = 250,
+        -- show line blame after diagnostics
+        virt_text_priority = 5000,
       },
       current_line_blame_formatter = '    <author>, <author_time:%R> • <summary>',
       current_line_blame_formatter_nc = '    You, <author_time:%R>',
