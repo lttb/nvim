8eb7e54766a6852f16f92dbfd41fef508c1a6004
Artur Kenzhaev <kenzhaev.artur@gmail.com>
2023-11-04 20:01:18 +0000

    feat(lsp): support limited autocomplete options when they're restricted by lsp
    for example, in typescript, in this snippet
    ```ts
    const Test = (props: {hello: 'world', foo: 'bar'}) => null
    
    <Test .../>
    Test({...})
    ```
    only available options in autocomplete should be `hello` and `foo`, ignoring other sources

--- lua/lttb/plugins/lsp.lua
+++ lua/lttb/plugins/lsp.lua
@@ -105,6 +105,8 @@ local function config()
     },
   })
 
+  -- {{{ CMP
+
   local has_words_before = function()
     local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
     return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
@@ -114,11 +116,102 @@ local function config()
   local compare = cmp.config.compare
   local lspkind = require('lspkind')
   local luasnip = require('luasnip')
+  local defaults = require('cmp.config.default')()
+
+  local ts_utils = require('nvim-treesitter.ts_utils')
+
+  local function is_in_typescript_context()
+    local current_node = ts_utils.get_node_at_cursor()
+    if not current_node then
+      return false
+    end
+
+    while current_node do
+      local node_type = current_node:type()
+      -- Adjust the node types according to the Treesitter grammar
+      if
+        node_type == 'object'
+        or node_type == 'object_type'
+        or node_type == 'type_literal'
+        or node_type == 'type_annotation'
+        or node_type == 'jsx_element'
+        or node_type == 'jsx_self_closing_element'
+        or node_type == 'jsx_attribute'
+        or node_type == 'jsx_expression'
+      then
+        return true
+      end
+      current_node = current_node:parent()
+    end
+    return false
+  end
+
+  local default_sources = {
+    -- { name = 'copilot', group_index = 2 },
+    {
+      name = 'nvim_lsp',
+    },
+    {
+      name = 'luasnip',
+      keyword_length = 3,
+    },
+    {
+      name = 'path',
+      keyword_length = 3,
+    },
+    {
+      name = 'buffer',
+      keyword_length = 3,
+    },
+    {
+      name = 'rg',
+      keyword_length = 2,
+    },
+  }
+
+  vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+    pattern = { '*.ts', '*.tsx' },
+    callback = function()
+      if is_in_typescript_context() then
+        cmp.setup.buffer({ sources = { { name = 'nvim_lsp' } } })
+      else
+        cmp.setup.buffer({
+          sources = default_sources,
+        })
+      end
+    end,
+  })
 
   cmp.setup({
+    snippet = {
+      expand = function(args)
+        luasnip.lsp_expand(args.body)
+      end,
+    },
+
+    enabled = function()
+      -- Set of commands where cmp will be disabled
+      local disabled = {
+        IncRename = true,
+      }
+      -- Get first word of cmdline
+      local cmd = vim.fn.getcmdline():match('%S+')
+      -- Return true if cmd isn't disabled
+      -- else call/return cmp.close(), which returns false
+      return not disabled[cmd] or cmp.close()
+    end,
+
     mapping = {
       -- `Enter` key to confirm completion
       ['<CR>'] = cmp.mapping.confirm({ select = true }),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<Esc>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.abort()
+        else
+          fallback()
+        end
+      end, { 'i', 's' }),
 
       -- Ctrl+Space to trigger completion menu
       ['<C-Space>'] = cmp.mapping.complete(),
@@ -139,36 +232,14 @@ local function config()
         else
           fallback()
         end
-      end, { 'i', 's' }),
+      end, { 'i', 's', 'c' }),
     },
 
     experimental = {
       ghost_text = true, -- this feature conflict with copilot.vim's preview.
     },
 
-    sources = cmp.config.sources({
-      -- { name = 'copilot', group_index = 2 },
-      {
-        name = 'nvim_lsp',
-        keyword_length = 1,
-      },
-      {
-        name = 'luasnip',
-        keyword_length = 3,
-      },
-      {
-        name = 'path',
-        keyword_length = 3,
-      },
-      {
-        name = 'buffer',
-        keyword_length = 3,
-      },
-      {
-        name = 'rg',
-        keyword_length = 1,
-      },
-    }),
+    sources = cmp.config.sources(default_sources),
 
     sorting = {
       comparators = {
@@ -223,6 +294,8 @@ local function config()
       },
     }),
   })
+
+  -- }}}
 end
 
 return {
