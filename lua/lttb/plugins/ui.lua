99c22b73c1210797bed4398ae3f188b15548efa0
Artur Kenzhaev <kenzhaev.artur@gmail.com>
2023-11-07 15:52:40 +0000

    feat(editor): minor tweaks

--- lua/lttb/plugins/ui.lua
+++ lua/lttb/plugins/ui.lua
@@ -92,21 +92,13 @@ return {
     {
       'folke/noice.nvim',
       opts = {
-        lsp = {
-          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-          override = {
-            ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-            ['vim.lsp.util.stylize_markdown'] = true,
-            ['cmp.entry.get_documentation'] = true,
-          },
-        },
         -- you can enable a preset for easier configuration
         presets = {
           bottom_search = true, -- use a classic bottom cmdline for search
           command_palette = true, -- position the cmdline and popupmenu together
           long_message_to_split = true, -- long messages will be sent to a split
           inc_rename = false, -- enables an input dialog for inc-rename.nvim
-          lsp_doc_border = false, -- add a border to hover docs and signature help
+          lsp_doc_border = true, -- add a border to hover docs and signature help
         },
       },
       dependencies = {
@@ -349,9 +341,8 @@ return {
 
     {
       'mvllow/modes.nvim',
-      enabled = false,
       opts = {
-        line_opacity = 0.05,
+        line_opacity = 0.1,
       },
     },
 
