bdbdde6dac55d95ecad672538fb89a9b1d751ab1
Artur Kenzhaev <kenzhaev.artur@gmail.com>
2023-11-07 19:08:07 +0000

    feat: add none-ls

---
+++ lua/lttb/plugins/none-ls.lua
@@ -0,0 +1,38 @@
+local utils = require('lttb.utils')
+
+if utils.is_vscode() then
+  return {}
+end
+
+return {
+  -- none-ls
+  {
+    'nvimtools/none-ls.nvim',
+    dependencies = { 'mason.nvim' },
+    opts = function()
+      local null_ls = require('null-ls')
+
+      return {
+        sources = {
+          null_ls.builtins.code_actions.eslint_d,
+          null_ls.builtins.code_actions.cspell,
+
+          null_ls.builtins.formatting.stylua,
+          null_ls.builtins.formatting.eslint_d,
+          null_ls.builtins.formatting.markdownlint,
+          null_ls.builtins.formatting.prettierd,
+
+          null_ls.builtins.diagnostics.eslint,
+          null_ls.builtins.diagnostics.cspell.with({
+            diagnostics_postprocess = function(diagnostic)
+              diagnostic.severity = vim.diagnostic.severity['INFO']
+            end,
+          }),
+          null_ls.builtins.diagnostics.markdownlint,
+
+          null_ls.builtins.completion.spell,
+        },
+      }
+    end,
+  },
+}
