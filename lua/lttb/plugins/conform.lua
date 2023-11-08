ef30c9ac2663b77ea6c615bfee2a9dcb47515ba8
Artur Kenzhaev <kenzhaev.artur@gmail.com>
2023-11-08 13:05:08 +0000

    feat(conform): enable it back

--- lua/lttb/plugins/conform.lua
+++ lua/lttb/plugins/conform.lua
@@ -4,10 +4,20 @@ if utils.is_vscode() then
   return {}
 end
 
+local js = { { 'prettierd', 'prettier' }, 'eslint_d' }
+
 return {
   'stevearc/conform.nvim',
+  enabled = true,
   opts = {
     formatters_by_ft = {
+      lua = { 'stylua' },
+      javascript = js,
+      typescript = js,
+      typescriptreact = js,
+      json = js,
+      markdown = js,
+
       -- Use the "_" filetype to run formatters on filetypes that don't
       -- have other formatters configured.
       ['_'] = { 'trim_whitespace' },
