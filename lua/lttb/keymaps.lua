85d06d3fc5b9dffdea2f58c00bd6baaa2796e17c
Artur Kenzhaev <kenzhaev.artur@gmail.com>
2023-11-13 01:01:38 +0000

    feat(keymaps): remape page scroll

--- lua/lttb/keymaps.lua
+++ lua/lttb/keymaps.lua
@@ -97,6 +97,9 @@ vim.keymap.set('n', '<C-Down>', '<cmd>resize -2<cr>', { desc = 'Decrease window 
 vim.keymap.set('n', '<C-Left>', '<cmd>vertical resize -2<cr>', { desc = 'Decrease window width' })
 vim.keymap.set('n', '<C-Right>', '<cmd>vertical resize +2<cr>', { desc = 'Increase window width' })
 
+vim.keymap.set('n', '<C-D>', '25j', { desc = 'Better Scroll Down' })
+vim.keymap.set('n', '<C-U>', '25k', { desc = 'Better Scroll Up' })
+
 -- Clear search with <esc>
 vim.keymap.set({ 'i', 'n' }, '<esc>', '<cmd>noh<cr><esc>', { desc = 'Escape and clear hlsearch' })
 
