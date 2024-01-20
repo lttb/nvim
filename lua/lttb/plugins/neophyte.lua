81071150499704a2500bc1bd9dcf8b396448c87e
Artur Kenzhaev <kenzhaev.artur@gmail.com>
2024-01-20 12:21:00 +0000

    feat: try neophyte

---
+++ lua/lttb/plugins/neophyte.lua
@@ -0,0 +1,17 @@
+return {
+  'tim-harding/neophyte',
+  enabled = false,
+  event = 'VeryLazy',
+  opts = {
+    fonts = {
+      {
+        name = 'Fira Code',
+      },
+    },
+
+    font_size = {
+      kind = 'width', -- 'width' | 'height'
+      size = 18,
+    },
+  },
+}
