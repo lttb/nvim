f833a0db4cb8629480c9109685523c24fb2dc80d
Artur Kenzhaev <kenzhaev.artur@gmail.com>
2024-02-18 13:01:31 +0000

    feat: update text nav shortcuts

--- lua/lttb/dev/osx-text-nav.lua
+++ lua/lttb/dev/osx-text-nav.lua
@@ -1,36 +1,35 @@
 local M = {}
 
 function M.setup()
-  vim.keymap.set('x', '<BS>', 'd')
+  vim.api.nvim_set_keymap('n', '<A-Left>', 'b', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('n', '<A-Right>', 'w', { noremap = true, silent = true })
+  -- For moving word back and forth in insert mode, consider using <C-Left> and <C-Right> which are more native to insert mode operations:
+  vim.api.nvim_set_keymap('i', '<A-Left>', '<C-Left>', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('i', '<A-Right>', '<C-Right>', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('v', '<A-Left>', 'b', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('v', '<A-Right>', 'w', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('t', '<A-Left>', '<C-\\><C-N>b', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('t', '<A-Right>', '<C-\\><C-N>w', { noremap = true, silent = true })
 
-  local function native_nav(key, ncmd, icmd, xcmd)
-    vim.keymap.set('n', key, ncmd, { remap = true })
-    vim.keymap.set(
-      'i',
-      key,
-      '<cmd>set eventignore=InsertLeave<cr><esc>' .. key .. '<cmd>set eventignore=""<cr>' .. (icmd or ''),
-      { remap = true }
-    )
-    if xcmd then
-      vim.keymap.set('x', key, xcmd, { remap = true })
-    end
-  end
+  -- Delete a word: Option + Backspace
+  vim.api.nvim_set_keymap('i', '<A-BS>', '<C-W>', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('n', '<A-BS>', 'db', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('v', '<A-BS>', 'd', { noremap = true, silent = true })
 
-  local wm = ''
+  -- Select to the beginning of the word with Option+Shift+Left
+  vim.api.nvim_set_keymap('n', '<A-S-Left>', 'vb', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('i', '<A-S-Left>', '<Esc>vb', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('v', '<A-S-Left>', 'b', { noremap = true, silent = true })
 
-  native_nav('<M-BS>', 'l"_db', 'i')
-  native_nav('<M-DEL>', wm .. 'e"_da' .. wm .. 'w', 'i')
+  -- Undo with Cmd+Z
+  vim.api.nvim_set_keymap('n', '<D-z>', 'u', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('i', '<D-z>', '<C-o>u', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('v', '<D-z>', '<C-o>u', { noremap = true, silent = true })
 
-  native_nav('<S-Left>', 'vh', '', 'h')
-  native_nav('<S-Right>', 'vl', '', 'l')
-  native_nav('<S-Up>', 'vk', '', 'k')
-  native_nav('<S-Down>', 'vj', '', 'j')
-
-  native_nav('<M-Left>', wm .. 'b', 'i')
-  native_nav('<M-Right>', wm .. 'e', 'a')
-
-  native_nav('<M-S-Left>', wm .. 'ev' .. wm .. 'b', '', ',b')
-  native_nav('<M-S-Right>', 'vi' .. wm .. 'e', '', ',w')
+  -- Redo with Cmd+Shift+Z
+  vim.api.nvim_set_keymap('n', '<S-D-z>', ':redo<CR>', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('i', '<S-D-z>', '<C-o>:redo<CR>', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('v', '<S-D-z>', '<C-o>:redo<CR>', { noremap = true, silent = true })
 end
 
 return M
