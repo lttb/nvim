4c80ec8e558e5dfa53e7a74b96180d8121dd1b62
Artur Kenzhaev <kenzhaev.artur@gmail.com>
2024-02-20 17:30:34 +0000

    feat: add no-neck-pain.nvim

--- lua/lttb/plugins/ui.lua
+++ lua/lttb/plugins/ui.lua
@@ -430,5 +430,17 @@ return {
         },
       },
     },
+
+    {
+      'shortcuts/no-neck-pain.nvim',
+      enabled = false,
+      opts = {
+        width = 120,
+
+        autocmds = {
+          enableOnVimEnter = true,
+        },
+      },
+    },
   },
 }
