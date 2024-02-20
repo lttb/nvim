d8d6ad24b994803d7109b632f4081462a04e857f
Artur Kenzhaev <kenzhaev.artur@gmail.com>
2024-02-20 17:30:15 +0000

    feat(neo-tree): update settings

--- lua/lttb/plugins/neo-tree.lua
+++ lua/lttb/plugins/neo-tree.lua
@@ -6,7 +6,6 @@ end
 
 return {
   {
-    enabled = false,
     'nvim-neo-tree/neo-tree.nvim',
     cmd = { 'Neotree' },
     keys = {
@@ -50,8 +49,6 @@ return {
           F = 'find_in_dir',
           O = 'system_open',
           Y = 'copy_selector',
-          h = 'parent_or_close',
-          l = 'child_or_open',
           o = 'open',
         },
         fuzzy_finder_mappings = { -- define keymaps for filter popup window in fuzzy_finder_mode
@@ -87,26 +84,6 @@ return {
       commands = {
         system_open = function(state)
           vim.ui.open(state.tree:get_node():get_id())
-        end,
-        parent_or_close = function(state)
-          local node = state.tree:get_node()
-          if (node.type == 'directory' or node:has_children()) and node:is_expanded() then
-            state.commands.toggle_node(state)
-          else
-            require('neo-tree.ui.renderer').focus_node(state, node:get_parent_id())
-          end
-        end,
-        child_or_open = function(state)
-          local node = state.tree:get_node()
-          if node.type == 'directory' or node:has_children() then
-            if not node:is_expanded() then -- if unexpanded, expand
-              state.commands.toggle_node(state)
-            else -- if expanded and has children, seleect the next child
-              require('neo-tree.ui.renderer').focus_node(state, node:get_child_ids()[1])
-            end
-          else -- if not a directory just open it
-            state.commands.open(state)
-          end
         end,
         copy_selector = function(state)
           local notify = require('notify')
