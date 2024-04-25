c0bce747b70464175ac446a62c94ef36fd873de1
Artur Kenzhaev <kenzhaev.artur@gmail.com>
2024-04-25 22:28:04 +0100

    fix(vscode): fix find reference command

--- lua/lttb/settings/vscode.lua
+++ lua/lttb/settings/vscode.lua
@@ -3,7 +3,7 @@ vim.api.nvim_set_keymap('n', 'gc', '<Plug>VSCodeCommentary', {})
 vim.api.nvim_set_keymap('o', 'gc', '<Plug>VSCodeCommentary', {})
 vim.api.nvim_set_keymap('n', 'gcc', '<Plug>VSCodeCommentaryLine', {})
 
-vim.api.nvim_set_keymap('n', 'gr', ':call VSCodeNotify("workbench.action.goToReferences")<cr>', {})
+vim.api.nvim_set_keymap('n', 'gr', ':call VSCodeNotify("editor.action.goToReferences")<cr>', {})
 
 vim.api.nvim_set_keymap('n', '<leader><leader>', ':call VSCodeNotify("find-it-faster.findFiles")<cr>', {})
 vim.api.nvim_set_keymap('n', '/', ':call VSCodeNotify("go-to-fuzzy.find")<cr>', {})
