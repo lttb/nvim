b9b9d191a8a8100543e81412416b212a52596fc7
Artur Kenzhaev <kenzhaev.artur@gmail.com>
2024-06-15 14:53:51 +0100

    feat(nvim-tree): disable open on init

--- lua/lttb/plugins/nvim-tree.lua
+++ lua/lttb/plugins/nvim-tree.lua
@@ -43,24 +43,24 @@ return {
         },
       }
     end,
-    init = function()
-      vim.api.nvim_create_autocmd({ 'VimEnter' }, {
-        -- it should be "nested" not to show the number column
-        -- @see https://github.com/nvim-neo-tree/neo-tree.nvim/issues/1106
-        nested = true,
-        callback = function(data)
-          if not utils.should_open_sidebar(data) then
-            return
-          end
-
-          vim.schedule(function()
-            require('nvim-tree.api').tree.toggle({
-              focus = false,
-            })
-          end)
-        end,
-      })
-    end,
+    -- init = function()
+    --   vim.api.nvim_create_autocmd({ 'VimEnter' }, {
+    --     -- it should be "nested" not to show the number column
+    --     -- @see https://github.com/nvim-neo-tree/neo-tree.nvim/issues/1106
+    --     nested = true,
+    --     callback = function(data)
+    --       if not utils.should_open_sidebar(data) then
+    --         return
+    --       end
+    --
+    --       vim.schedule(function()
+    --         require('nvim-tree.api').tree.toggle({
+    --           focus = false,
+    --         })
+    --       end)
+    --     end,
+    --   })
+    -- end,
     opts = {
       prefer_startup_root = true,
       sync_root_with_cwd = true,
