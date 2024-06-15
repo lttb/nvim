25ca786c26ea91cfa434f287220810e484d3439e
Artur Kenzhaev <kenzhaev.artur@gmail.com>
2024-06-15 14:06:49 +0100

    feat: update toggle_float

--- lua/lttb/dev/toggle_floats.lua
+++ lua/lttb/dev/toggle_floats.lua
@@ -1,5 +1,3 @@
-local utils = require('lttb.utils')
-
 local M = {}
 
 local registry = {}
@@ -15,27 +13,10 @@ function M.toggle_floats(name, callback)
 
   local wins = vim.api.nvim_list_wins()
 
-  -- utils.log(wins)
-
   if #reg.hidden_w > 0 then
     for _, w in ipairs(reg.hidden_w) do
       vim.api.nvim_win_set_config(w, { hide = false })
     end
-
-    local widest = 0
-    local widest_wid
-
-    -- for _, w in ipairs(wins) do
-    --   local win_config = vim.api.nvim_win_get_config(w)
-
-    --   print(w)
-
-    --   -- utils.log(win_config)
-
-    --   -- if win_config.width > widest then
-    --   --   widest_wid = w
-    --   -- end
-    -- end
 
     if reg.last_w then
       vim.api.nvim_set_current_win(reg.last_w)
@@ -54,15 +35,13 @@ function M.toggle_floats(name, callback)
   reg.last_w = vim.api.nvim_get_current_win()
 
   for id, w in ipairs(wins) do
-    -- print(_, w)
     local win_config = vim.api.nvim_win_get_config(w)
-    -- utils.log(win_config)
+
     if
       win_config.relative == 'editor'
       -- ignore mini map (TODO: find a better detection way)
       and win_config.zindex ~= 10
     then
-      -- utils.log(win_config)
       vim.api.nvim_win_set_config(w, { hide = true })
       table.insert(reg.hidden_w, w)
     end
