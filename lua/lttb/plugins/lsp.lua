4319696fe50a09aec5f2bc4dcaab60b4ee7048be
Artur Kenzhaev <kenzhaev.artur@gmail.com>
2025-01-14 22:21:28 +0000

    feat(lsp): disable jsonls formatting

--- lua/lttb/plugins/lsp.lua
+++ lua/lttb/plugins/lsp.lua
@@ -176,6 +176,10 @@ local function config()
     },
 
     jsonls = {
+      on_attach = function(client, bufnr)
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentRangeFormattingProvider = false
+      end,
       settings = {
         json = {
           schemas = require('schemastore').json.schemas(),
