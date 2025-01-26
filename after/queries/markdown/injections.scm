d228601d2f9eb6112505642c18f60c848e31998e
Artur Kenzhaev <kenzhaev.artur@gmail.com>
2025-01-26 23:56:25 +0000

    feat: add tsx treesitter injections

--- after/queries/markdown/injections.scm
+++ after/queries/markdown/injections.scm
@@ -1,3 +1,4 @@
 ; extends
+
 ((inline) @_inline (#match? @_inline "^\(import\|export\)")) @tsx
 (html_block) @tsx
---
+++ after/queries/tsx/highlights.scm
@@ -0,0 +1,12 @@
+; extends
+
+(
+ (variable_declarator
+    name: (identifier) @variable)
+  (#set! "priority" 128))
+
+(
+  (import_specifier
+    name: (identifier) @variable)
+  (#set! "priority" 128)
+)
