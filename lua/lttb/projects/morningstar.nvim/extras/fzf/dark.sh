fcd16a611817a356e491128521134ab763d3f41d
Artur Kenzhaev <kenzhaev.artur@gmail.com>
2025-02-12 13:23:50 +0000

    feat(morningstar): add extras

---
+++ lua/lttb/projects/morningstar.nvim/extras/fzf/dark.sh
@@ -0,0 +1,4 @@
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS""\
+ --color=bg+:#404040,bg:#24282e,spinner:#66A5AD,hl:#6099C0""\
+ --color=fg:#BBBBBB,header:#6099C0,info:#B77E64,pointer:#66A5AD""\
+ --color=marker:#66A5AD,fg+:#BBBBBB,prompt:#B77E64,hl+:#61ABDA"
---
+++ lua/lttb/projects/morningstar.nvim/extras/fzf/light.sh
@@ -0,0 +1,4 @@
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS""\
+ --color=bg+:#dfdad9,bg:#ffffff,spinner:#d7827e,hl:#56949f""\
+ --color=fg:#575279,header:#56949f,info:#ea9d34,pointer:#d7827e""\
+ --color=marker:#d7827e,fg+:#575279,prompt:#ea9d34,hl+:#56949f"
