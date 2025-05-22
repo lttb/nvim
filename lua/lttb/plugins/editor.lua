20c397ff2ea90abc9cea23ee31dcde8d501f5c75
Artur Kenzhaev <kenzhaev.artur@gmail.com>
2025-05-22 08:39:15 +0100

    feat: add image.nvim

--- lua/lttb/plugins/editor.lua
+++ lua/lttb/plugins/editor.lua
@@ -110,4 +110,12 @@ return {
       { '<M-i>', ":lua require('bufjump').forward_same_buf()<cr>",  mode = { 'n' }, silent = true },
     },
   },
+
+  {
+    '3rd/image.nvim',
+    build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+    opts = {
+      processor = 'magick_cli',
+    },
+  },
 }
