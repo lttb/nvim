fb01195b696265c3bb15dce4bf77a6b1caf464ea
Artur Kenzhaev <kenzhaev.artur@gmail.com>
2025-07-23 11:41:07 +0100

    feat(fyler): update options

--- lua/lttb/plugins/_/fyler.lua
+++ lua/lttb/plugins/_/fyler.lua
@@ -8,7 +8,7 @@ return {
   {
     'A7Lavinraj/fyler.nvim',
     dependencies = { 'echasnovski/mini.icons' },
-    branch = 'stable',
+    branch = 'main',
     opts = {
       auto_confirm_simple_edits = true,
       close_on_select = false,
@@ -17,8 +17,30 @@ return {
           width = 0.25,
           height = 1,
           kind = 'split:left',
+          win_opts = {
+            number = false,
+            relativenumber = false,
+          },
         },
       },
     },
+    init = function()
+      local is_shown = false
+
+      vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+        -- it should be "nested" not to show the number column
+        -- @see https://github.com/nvim-neo-tree/neo-tree.nvim/issues/1106
+        nested = true,
+        callback = function(data)
+          if not utils.should_open_sidebar(data) or is_shown then
+            return
+          end
+
+          -- open the tree but don't focus it
+          is_shown = true
+          vim.cmd('Fyler')
+        end,
+      })
+    end,
   },
 }
