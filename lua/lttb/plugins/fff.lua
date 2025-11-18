789992568c03b4971d9e229e86205399cef5f765
Artur Kenzhaev <kenzhaev.artur@gmail.com>
2025-11-18 17:26:32 +0000

    feat: fff -> snacks

--- lua/lttb/plugins/fff.lua
+++ lua/lttb/plugins/fff.lua
@@ -13,21 +13,21 @@ return {
       -- pass here all the options
     },
     keys = {
-      {
-        '<leader><leader>', -- try it if you didn't it is a banger keybinding for a picker
-        function()
-          if utils.is_dotfiles() then
-            require('fzf-lua').git_files({
-              cmd = 'git ls-files -c -o --exclude-standard',
-            })
-
-            return
-          end
-
-          require('fff').find_files()
-        end,
-        desc = 'Toggle FFF',
-      },
+      -- {
+      --   '<leader><leader>', -- try it if you didn't it is a banger keybinding for a picker
+      --   function()
+      --     if utils.is_dotfiles() then
+      --       require('fzf-lua').git_files({
+      --         cmd = 'git ls-files -c -o --exclude-standard',
+      --       })
+      --
+      --       return
+      --     end
+      --
+      --     require('fff').find_files()
+      --   end,
+      --   desc = 'Toggle FFF',
+      -- },
     },
   },
 }
