0c0a8caafa4e198dd67407ae5934f954df7605e5
Artur Kenzhaev <kenzhaev.artur@gmail.com>
2025-12-10 15:51:03 +0000

    feat(fff): use cargo build

--- lua/lttb/plugins/fff.lua
+++ lua/lttb/plugins/fff.lua
@@ -7,12 +7,7 @@ end
 return {
   {
     'dmtrKovalenko/fff.nvim',
-    branch = 'main',
-    build = function()
-      -- this will download prebuild binary or try to use existing rustup toolchain to build from source
-      -- (if you are using lazy you can use gb for rebuilding a plugin if needed)
-      require('fff.download').download_or_build_binary()
-    end,
+    build = 'cargo build --release',
     lazy = false,
     opts = {},
     keys = {
