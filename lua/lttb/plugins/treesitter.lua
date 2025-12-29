6c17c65e4d97382fed3e9a3dbd62742f2f164acb
Artur Kenzhaev <kenzhaev.artur@gmail.com>
2025-12-29 11:09:12 +0000

    feat: migrate to main treesitter

--- lua/lttb/plugins/treesitter.lua
+++ lua/lttb/plugins/treesitter.lua
@@ -3,37 +3,27 @@ local utils = require('lttb.utils')
 return {
   {
     'nvim-treesitter/nvim-treesitter',
-    version = false, -- last release is way too old and doesn't work on Windows
+    branch = 'main',
     build = ':TSUpdate',
     event = { 'LazyFile', 'VeryLazy' },
-    lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
-    init = function(plugin)
-      -- @see https://github.com/LazyVim/LazyVim/blob/d0c366e4d861b848bdc710696d5311dca2c6d540/lua/lazyvim/plugins/treesitter.lua#L21-L28
-      -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
-      -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
-      -- no longer trigger the **nvim-treesitter** module to be loaded in time.
-      -- Luckily, the only things that those plugins need are the custom queries, which we make available
-      -- during startup.
-      require('lazy.core.loader').add_to_rtp(plugin)
-      require('nvim-treesitter.query_predicates')
-    end,
+    lazy = false,
     cmd = { 'TSUpdateSync', 'TSUpdate', 'TSInstall' },
     config = function()
-      require('nvim-treesitter.configs').setup({
+      require('nvim-treesitter').setup({
         -- Add languages to be installed here that you want installed for treesitter
         ensure_installed = { 'lua', 'typescript', 'rust', 'go', 'python' },
 
         auto_install = true,
 
-        refactor = {
-          highlight_definitions = {
-            enable = false,
-            -- Set to false if you have an `updatetime` of ~100.
-            clear_on_cursor_move = true,
-          },
-
-          highlight_current_scope = { enable = false },
-        },
+        -- refactor = {
+        --   highlight_definitions = {
+        --     enable = false,
+        --     -- Set to false if you have an `updatetime` of ~100.
+        --     clear_on_cursor_move = true,
+        --   },
+        --
+        --   highlight_current_scope = { enable = false },
+        -- },
 
         highlight = {
           enable = not utils.is_vscode(),
@@ -41,95 +31,95 @@ return {
           additional_vim_regex_highlighting = false,
         },
 
-        indent = {
-          enable = true,
-        },
+        -- indent = {
+        --   enable = true,
+        -- },
 
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = 'gnn',
-            node_incremental = 'grn',
-            scope_incremental = 'grc',
-            node_decremental = 'grm',
-          },
-        },
+        -- incremental_selection = {
+        --   enable = true,
+        --   keymaps = {
+        --     init_selection = 'gnn',
+        --     node_incremental = 'grn',
+        --     scope_incremental = 'grc',
+        --     node_decremental = 'grm',
+        --   },
+        -- },
 
-        textsubjects = {
-          enable = true,
-          prev_selection = ',', -- (Optional) keymap to select the previous selection
-          keymaps = {
-            ['.'] = 'textsubjects-smart',
-            [';'] = 'textsubjects-container-outer',
-            ['i;'] = 'textsubjects-container-inner',
-          },
-        },
+        -- textsubjects = {
+        --   enable = true,
+        --   prev_selection = ',', -- (Optional) keymap to select the previous selection
+        --   keymaps = {
+        --     ['.'] = 'textsubjects-smart',
+        --     [';'] = 'textsubjects-container-outer',
+        --     ['i;'] = 'textsubjects-container-inner',
+        --   },
+        -- },
 
-        textobjects = {
-          select = {
-            enable = true,
-
-            -- Automatically jump forward to textobj, similar to targets.vim
-            lookahead = true,
-
-            keymaps = {
-              -- You can use the capture groups defined in textobjects.scm
-              -- ['af'] = '@function.outer',
-              -- ['if'] = '@function.inner',
-              -- ['as'] = '@statement.outer',
-              -- ['is'] = '@statement.inner',
-              -- ['ac'] = '@class.outer',
-              -- ['ic'] = '@class.inner',
-            },
-          },
-
-          swap = {
-            enable = true,
-            swap_next = {
-              ['<leader>a'] = '@parameter.inner',
-            },
-            swap_previous = {
-              ['<leader>A'] = '@parameter.inner',
-            },
-          },
-
-          move = {
-            enable = true,
-            set_jumps = true, -- whether to set jumps in the jumplist
-            goto_next_start = {
-              [']m'] = '@function.outer',
-              [']]'] = {
-                query = '@class.outer',
-                desc = 'Next class start',
-              },
-              [']p'] = '@parameter.inner',
-            },
-            goto_next_end = {
-              [']M'] = '@function.outer',
-              [']['] = '@class.outer',
-              [']P'] = '@parameter.inner',
-            },
-            goto_previous_start = {
-              ['[m'] = '@function.outer',
-              ['[['] = '@class.outer',
-              ['[p'] = '@parameter.inner',
-            },
-            goto_previous_end = {
-              ['[M'] = '@function.outer',
-              ['[]'] = '@class.outer',
-              ['[P'] = '@parameter.inner',
-            },
-          },
-
-          lsp_interop = {
-            enable = true,
-            border = 'none',
-            peek_definition_code = {
-              ['<leader>df'] = '@function.outer',
-              ['<leader>dF'] = '@class.outer',
-            },
-          },
-        },
+        -- textobjects = {
+        --   select = {
+        --     enable = true,
+        --
+        --     -- Automatically jump forward to textobj, similar to targets.vim
+        --     lookahead = true,
+        --
+        --     keymaps = {
+        --       -- You can use the capture groups defined in textobjects.scm
+        --       -- ['af'] = '@function.outer',
+        --       -- ['if'] = '@function.inner',
+        --       -- ['as'] = '@statement.outer',
+        --       -- ['is'] = '@statement.inner',
+        --       -- ['ac'] = '@class.outer',
+        --       -- ['ic'] = '@class.inner',
+        --     },
+        --   },
+        --
+        --   swap = {
+        --     enable = true,
+        --     swap_next = {
+        --       ['<leader>a'] = '@parameter.inner',
+        --     },
+        --     swap_previous = {
+        --       ['<leader>A'] = '@parameter.inner',
+        --     },
+        --   },
+        --
+        --   move = {
+        --     enable = true,
+        --     set_jumps = true, -- whether to set jumps in the jumplist
+        --     goto_next_start = {
+        --       [']m'] = '@function.outer',
+        --       [']]'] = {
+        --         query = '@class.outer',
+        --         desc = 'Next class start',
+        --       },
+        --       [']p'] = '@parameter.inner',
+        --     },
+        --     goto_next_end = {
+        --       [']M'] = '@function.outer',
+        --       [']['] = '@class.outer',
+        --       [']P'] = '@parameter.inner',
+        --     },
+        --     goto_previous_start = {
+        --       ['[m'] = '@function.outer',
+        --       ['[['] = '@class.outer',
+        --       ['[p'] = '@parameter.inner',
+        --     },
+        --     goto_previous_end = {
+        --       ['[M'] = '@function.outer',
+        --       ['[]'] = '@class.outer',
+        --       ['[P'] = '@parameter.inner',
+        --     },
+        --   },
+        --
+        --   lsp_interop = {
+        --     enable = true,
+        --     border = 'none',
+        --     peek_definition_code = {
+        --       ['<leader>df'] = '@function.outer',
+        --       ['<leader>dF'] = '@class.outer',
+        --     },
+        --   },
+        -- },
       })
 
       -- @see https://phelipetls.github.io/posts/mdx-syntax-highlight-treesitter-nvim/
@@ -143,13 +133,13 @@ return {
       vim.treesitter.language.register('tsx', { 'typescript', 'typescriptreact' })
     end,
     dependencies = {
-      'nvim-treesitter/nvim-treesitter-refactor',
-      'nvim-treesitter/nvim-treesitter-textobjects',
-      'RRethy/nvim-treesitter-textsubjects',
-      {
-        cond = not utils.is_vscode(),
-        'nvim-treesitter/playground',
-      },
+      -- 'nvim-treesitter/nvim-treesitter-refactor',
+      -- 'nvim-treesitter/nvim-treesitter-textobjects',
+      -- 'RRethy/nvim-treesitter-textsubjects',
+      -- {
+      --   cond = not utils.is_vscode(),
+      --   'nvim-treesitter/playground',
+      -- },
       {
         enabled = false,
         cond = not utils.is_vscode(),
