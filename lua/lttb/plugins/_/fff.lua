9374fe68465755c42856ab80b14e86e74f6b94ae
Artur Kenzhaev <kenzhaev.artur@gmail.com>
2026-01-06 22:03:26 +0000

    feat(fff): disable

--- lua/lttb/plugins/_/fff.lua
+++ lua/lttb/plugins/_/fff.lua
@@ -8,7 +8,11 @@ return {
   {
     enabled = not utils.is_dotfiles(),
     'dmtrKovalenko/fff.nvim',
-    build = 'cargo build --release',
+    build = function()
+      -- this will download prebuild binary or try to use existing rustup toolchain to build from source
+      -- (if you are using lazy you can use gb for rebuilding a plugin if needed)
+      require('fff.download').download_or_build_binary()
+    end,
     lazy = false,
     opts = {},
     keys = {
