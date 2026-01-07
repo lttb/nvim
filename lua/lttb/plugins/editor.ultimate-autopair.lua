10c7036010d128e6a8336fd74a572e897e0f4320
Artur Kenzhaev <kenzhaev.artur@gmail.com>
2026-01-07 17:45:20 +0000

    feat(autopairs): update config

--- lua/lttb/plugins/editor.ultimate-autopair.lua
+++ lua/lttb/plugins/editor.ultimate-autopair.lua
@@ -4,7 +4,9 @@ return {
     event = { 'InsertEnter', 'CmdlineEnter' },
     branch = 'v0.6', --recommended as each new version will have breaking changes
     opts = {
-      --Config goes here
+      bs = {
+        enable = false,
+      },
     },
   },
 }
