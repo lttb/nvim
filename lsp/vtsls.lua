2f1e51d30bb8a8009ea052f4187fbbaf19703eea
Artur Kenzhaev <kenzhaev.artur@gmail.com>
2026-01-11 16:34:47 +0000

    feat(vtsls): update typescript preferences

--- lsp/vtsls.lua
+++ lsp/vtsls.lua
@@ -3,4 +3,16 @@ return {
     client.server_capabilities.documentFormattingProvider = false
     client.server_capabilities.documentRangeFormattingProvider = false
   end,
+
+  settings = {
+    typescript = {
+      preferences = {
+        importModuleSpecifier = 'non-relative',
+        importModuleSpecifierEnding = 'auto',
+
+        -- optional but commonly nice:
+        includePackageJsonAutoImports = 'auto',
+      },
+    },
+  },
 }
