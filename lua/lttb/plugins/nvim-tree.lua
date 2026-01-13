7e2f0989fb8a5809fb78b10b673879613983fa39
Artur Kenzhaev <kenzhaev.artur@gmail.com>
2026-01-13 10:48:50 +0000

    feat(nvimtree): minor update

--- lua/lttb/plugins/nvim-tree.lua
+++ lua/lttb/plugins/nvim-tree.lua
@@ -37,13 +37,13 @@ return {
 
           glyphs = {
             git = {
-              unstaged = '',
-              staged = '',
-              unmerged = '',
-              renamed = '',
-              untracked = '',
-              deleted = '',
-              ignored = '',
+              unstaged = ' ',
+              staged = ' ',
+              unmerged = ' ',
+              renamed = ' ',
+              untracked = ' ',
+              deleted = ' ',
+              ignored = ' ',
             },
           },
         },
