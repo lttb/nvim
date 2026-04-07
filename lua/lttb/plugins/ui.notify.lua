b95a2189cd5150f7930b2b254c850edcbbc2d285
Artur Kenzhaev <kenzhaev.artur@gmail.com>
2026-04-08 00:18:49 +0100

    feat: return notify

--- lua/lttb/plugins/ui.notify.lua
+++ lua/lttb/plugins/ui.notify.lua
@@ -1,1 +1,9 @@
-return {}
+return {
+  {
+    'rcarriga/nvim-notify',
+    opts = {
+      background_colour = vim.o.background == 'light' and '#ffffff' or '#000000',
+    },
+    event = 'VeryLazy',
+  },
+}
